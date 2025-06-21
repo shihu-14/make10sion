23dec5b317e29d966eaa404e6bc209dfbccfbbcb
UnABC <xiaotudou888@gmail.com>
2025-06-21 11:11:08 +0900

    common.hppを追加

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,32 +1,5 @@
 ﻿# include <Siv3D.hpp> // Siv3D v0.6.16
-# include "Block.hpp"
-# include "Board.hpp"
-
-// シーンの名前
-enum class State
-{
-	Title,
-	Battle,
-	Map,
-	Result,
-	Deck
-};
-
-// 共有するデータ
-struct GameData
-{
-	std::vector<Block> Deck;
-	int Layer = 0;
-	int HP = 80;
-	int MaxHP = 80;
-	int money = 0;
-	Board board;
-	long long status = 0; // 状態
-};
-
-using App = SceneManager<State, GameData>;
-
-
+# include "common.hpp"
 # include "Title.hpp"
 # include "Map.hpp"
 # include "Battle.hpp"
@@ -54,7 +27,7 @@ void Main()
 	manager.add<Battle>(State::Battle);
 	manager.add<Result>(State::Result);
 	manager.add<Deck>(State::Deck);
-	
+
 
 	//XXX:debug用
 	//開始シーンを指定する

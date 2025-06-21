ef03c0acd951cb7a5b1353ee36ed39eb069f85df
suzume oi <czc03505@outlook.jp>
2025-06-21 19:28:29 +0900

    title完成

--- src/Result.cpp
+++ src/Result.cpp
@@ -2,13 +2,15 @@
 using namespace std;
 
 Result::Result(const InitData& init) : IScene(init){
-	
+	Scene::SetBackground(Palette::White);
+	retry_rect = RectF{ 100, 400, 200, 100 };
 }
 
 
 
 void Result::update(){
-	
+	font(U"Clear!\n \n Score:{}").drawAt(200, Vec2{ Scene::Center().x,Scene::Center().y}, ColorF{0.2});
+
 }
 
 void Result::draw() const{
--- src/Result.hpp
+++ src/Result.hpp
@@ -1,11 +1,17 @@
 #ifndef Result_HPP
 #define Result_HPP
+#include <Siv3D.hpp> // OpenSiv3D v0.6.4
+#include "common.hpp"
 
 
 class Result : public App::Scene{
 private:
 	//Write private functions or varables here.
-	
+	const Font font{ FontMethod::MSDF, 80,Typeface::Bold };
+	Texture title_back{ U"" };
+	Texture retry{ U"" };
+	Rect retry_rect;
+	Rect title_rect;
 public:
 	Result(const InitData& init);
 	//Write public functions here.

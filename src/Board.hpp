30be2fe7e111d3c8a7b62c95436a219629147bff
haru018 <haru_silverdale@outlook.com>
2025-06-21 09:44:35 +0900

    Board.hpp追加

--- src/Board.hpp
+++ src/Board.hpp
@@ -1,17 +1,39 @@
 #ifndef Board_HPP
 #define Board_HPP
 
+#include <utility>
+#include <Siv3D.hpp>
 
-class Board : public App::Scene{
-private:
-	//Write private functions or varables here.
+class Board{
+private://Write private functions or varables here.
+
+	//variables
+	Grid<int32> grid_usage(Size{7,6}, -1);
+	Grid<char> grid_number(Size{7,6}, 0);
+	Grid<int32> grid_effect(Size{7,6}, 0);
+	Grid<std::pair<int32, int32>> grid_corrdinate(Size{7,6},pair<int32>);
+	std::array<int, 6> grid_multiply;
+	std::array<int, 42> num_on_grid;
+
+	//function
+	void PutBlock();
+	void TakeOutBlock();
+	void AddUsablePlace();
+	void ResetBoard();
+
+
 	
-public:
-	Board(const InitData& init);
-	//Write public functions here.
 	
-	void update() override;
-	void draw() const override;
-}
+public://Write public functions here.
+
+	//variables
+
+	//functions
+	void Update(int idx);
+	void SetStat();
+	std::pair<int32, int32> Confirm();
+	void PassBlock();
+	void DrawBoard();
+};
 
 #endif

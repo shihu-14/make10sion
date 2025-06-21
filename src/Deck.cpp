649151fa5796879215ae0d6784e20027d9f6a037
UnABC <xiaotudou888@gmail.com>
2025-06-21 14:17:08 +0900

    PassBlockの修正

--- src/Deck.cpp
+++ src/Deck.cpp
@@ -1,17 +1,26 @@
 #include "Deck.hpp"
+#include "common.hpp"
 using namespace std;
 
-Deck::Deck(const InitData& init) : IScene(init){
-	
+Deck::Deck(const InitData& init) : IScene(init) {
+	//test
+	Block block;
+	block = "2\n3";
+	getData().Deck.push_back(block);
+	block = "$42\n3+$";
+	getData().Deck.push_back(block);
+	for (int i = 0;i < getData().Deck.size();i++)
+		getData().Deck.at(i).SetPos( 100 + (i % 9) * 200, 200 + 250* (i / 9) ); //カードの位置を設定
 }
 
 
 
-void Deck::update(){
-	
+void Deck::update() {
+
 }
 
-void Deck::draw() const{
-	
+void Deck::draw() const {
+	for (int i = 0;i < getData().Deck.size();i++) 
+		getData().Deck.at(i).Draw(0, 0, 1.0, 0.0, 1.0);
 }
 

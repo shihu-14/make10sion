052383e89c0167d8e1ce40c37cfebe939547a98b
haru018 <haru_silverdale@outlook.com>
2025-06-22 13:02:27 +0900

    画像の読み込み等修正

--- src/Board.hpp
+++ src/Board.hpp
@@ -28,10 +28,11 @@ private:
 	const Point offset = {0,0};//Boardの左上の絶対座標(バトル時)
 	const Point offset_u = {0,0};//Boardの左上の絶対座標(アンロック時)(使わないかも)
 	const int32 cell_size = 50;
-	const Texture board_img{U"../image/banmen_kuuhaku.png"};
-	const Texture chosed_board_img{ U"../image/special_n.png" };
-	const Texture chosable_board_img{ U"../image/tile_kokodayo.png" };
-	const Texture board_frame_img{ U"../image/tile_frame.png" };
+	const Texture board_img{U"../../image/banmen_kuuhaku.png"};
+	const Texture chosed_board_img{ U"../../image/special_n.png" };
+	const Texture chosable_board_img{ U"../../image/tile_kokodayo.png" };
+	const Texture board_frame_img{ U"../../image/tile_frame.png" };
+	const Font font{ FontMethod::MSDF, 48, Typeface::Bold };
 	Array<Block> used_blocks;//盤面に出てきたブロックの配列. blockNumは「このインデックス+1」とする
 	Array<Point> block_hand_pos;//各ブロックの手札上の位置を保存
 	Array<int32> block_anim;//実質描画順	-1:盤面上に無い, 0:ボード上, 1:手札へ, 2:捨札へ, 3:アニメーション無し

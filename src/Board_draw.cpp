fa9579f0ea1f72b1728b2f7262233a057be66c8c
からし <176755471+karashi-0086A2@users.noreply.github.com>
2025-06-22 15:35:46 +0900

    ちょっと修正

--- src/Board_draw.cpp
+++ src/Board_draw.cpp
@@ -12,16 +12,16 @@ void Board::DrawOnlyBoard() const {//Boardの描画のみ
                 board_img.scaled(img_scale).drawAt(board_coordinate[i][j]);
             }
             if (board_effect_front[i][j] == 1) {
-                font(U"+1").drawAt(60, board_coordinate[i][j], ColorF{ 0.2 })
+                font(U"+1").drawAt(60, board_coordinate[i][j], ColorF{ 0.2 });
             }
             else if (board_effect_front[i][j] == 2) {
-                font(U"+2").drawAt(60, board_coordinate[i][j], ColorF{ 0.2 })
+                font(U"+2").drawAt(60, board_coordinate[i][j], ColorF{ 0.2 });
             }
             else if (board_effect_front[i][j] == 4) {
-                font(U"+4").drawAt(60, board_coordinate[i][j], ColorF{ 0.2 })
+                font(U"+4").drawAt(60, board_coordinate[i][j], ColorF{ 0.2 });
             }
             else if (board_effect_front[i][j] == 5) {
-                font(U"+5").drawAt(60, board_coordinate[i][j], ColorF{ 0.2 })
+                font(U"+5").drawAt(60, board_coordinate[i][j], ColorF{ 0.2 });
             }
         }
     }

1e9f895a6a0ca0a735bdcf17e0758c44f89c166f
haru018 <haru_silverdale@outlook.com>
2025-06-22 09:09:07 +0900

    アニメーション順序整理1

--- src/Board.cpp
+++ src/Board.cpp
@@ -46,32 +46,33 @@ void Board::Update(int32 idx){//idx : 0:バトル中, 1:リザルト(マス解�
 
 		Array<Block> Deck_board;//Deck_boardがprivateになっているので、publicにしてもらうまで暫定的に
 
-		for(int i=0;i<used_blocks.size();i++){//盤面上のブロックの描画
-			if(do_block_anim[i] == 3){//ボード上
-				//
-			}
-			else if(do_block_anim[i] == 0){
-				//
-			}
-			else if(do_block_anim[i] == 1){
-				BlockAnimation(used_blocks[i], block_hand_pos[i]);
-			}
-			else if(do_block_anim[i] == 2){
-				BlockAnimation(used_blocks[i], Point{100, 100});//捨て札の座標を指定
-			}
-			else{//-1
-				//
-			}
-			if(do_block_anim[i] >= 0){
-				if(do_block_anim[i] == 1){
-					BlockAnimation(used_blocks[i], block_hand_pos[i]);
-			    }
-				else if(do_block_anim[i] == 2){
-        			BlockAnimation(used_blocks[i], Point{100, 100});//捨て札の座標を指定
-    			}
-			}
-			used_blocks[i].Draw(used_blocks[i].GetPos(), 1.0, 0.0, 1.0);
-		}
+		//以下、描画処理
+		DrawBoard();//Boardの描画
+
+		Array<Block> Deck_board;//Deck_boardがprivateになっているので、publicにしてもらうまで暫定的に
+
+        for(int i=0;i<used_blocks.size();i++){//ボードに配置されているブロック
+            if(block_anim[i] == 0){
+                used_blocks[i].Draw(used_blocks[i].GetPos(), 1.0, 0.0, 1.0);
+            }
+        }
+        for(int i=0;i<used_blocks.size();i++){//手札へ移動するブロック
+            if(block_anim[i] == 1){
+                BlockAnimation(used_blocks[i], block_hand_pos[i]);
+                used_blocks[i].Draw(used_blocks[i].GetPos(), 1.0, 0.0, 1.0);
+            }
+        }
+        for(int i=0;i<used_blocks.size();i++){//捨札へ移動するブロック
+            if(block_anim[i] == 2){
+                BlockAnimation(used_blocks[i], Point{100, 100});//捨て札の座標を指定
+                used_blocks[i].Draw(used_blocks[i].GetPos(), 1.0, 0.0, 1.0);
+            }
+        }
+        for(int i=0;i<used_blocks.size();i++){//その他(ホールドされているブロックだけの筈)
+            if(block_anim[i] == 3){
+                used_blocks[i].Draw(used_blocks[i].GetPos(), 1.0, 0.0, 1.0);
+            }
+        }
 	}
 
 	else if(idx == 1){

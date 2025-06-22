62f612f442a34fc08c0b917796e96d06c23029e8
haru018 <haru_silverdale@outlook.com>
2025-06-22 14:02:11 +0900

    フレーム,サイズ修正(1)

--- src/Board.hpp
+++ src/Board.hpp
@@ -33,10 +33,11 @@ private:
 	const Point offset = {100,100};//Boardの左上の絶対座標(バトル時)
 	const Point offset_u = {0,0};//Boardの左上の絶対座標(アンロック時)(使わないかも)
 	const int32 cell_size = 100;
+	const int32 img_scale = 4;
 	const Texture board_img{U"../../image/banmen_kuuhaku.png"};
 	const Texture chosed_board_img{ U"../../image/special_n.png" };
 	const Texture chosable_board_img{ U"../../image/tile_kokodayo.png" };
-	const Texture board_frame_img{ U"../../image/tile_frame.png" };
+	const Texture board_frame_img{ U"../../image/tile_flame.png" };
 	const Font font{ FontMethod::MSDF, 48, Typeface::Bold };
 	Array<Block> used_blocks;//盤面に出てきたブロックの配列. blockNumは「このインデックス+1」とする
 	Array<Point> block_hand_pos;//各ブロックの手札上の位置を保存
